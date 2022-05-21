e0616f7d560bbce54981deaa32c113792153e3bb
zlr-zmm <81850357+zlr-zmm@users.noreply.github.com>
2022-05-21 17:11:42 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,17 @@
+document.write('<!-----------  公共js和css start----------->');
+document.write('' +
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bynotes/texiao/source/css/shubiao.css">\n' +
+    '<link rel="stylesheet" href="https://at.alicdn.com/t/font_3418311_0g8f2aycddgv.css" type="text/css">\n' +
+    '<link rel="apple-touch-icon" sizes="76x76" href="https://i.postimg.cc/RZv15Dmm/logo.jpg">\n' +
+    '<link rel="shortcut icon" href="https://i.postimg.cc/RZv15Dmm/logo.jpg" type="image/x-png">\n' +
+    '<link rel="stylesheet" href="css/mystyle.css">\n' +
+    '<!--\n' +
+    'Hi, It\'s Zmm. This is a good homepage theme.\n' +
+    'Github: https://github.com/zlr-zmm\n' +
+    '-->' +
+    '');
+
+
+
+
+
